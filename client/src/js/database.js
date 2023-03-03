62cdbd591422d20dcc8ac265caa3b2706611f813
DBENSAN <dftb97@gmail.com>
2023-03-02 21:28:18 -0800

    Adds database func

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,22 +14,22 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  const txtEditorDB = await openDB('jate', 1);
-  const transVar = txtEditorDB.transaction('jate', 'readwrite');
-  const storeVar = transVar.objectStore('jate');
-  const request = storeVar.put({ id: 1, value: content });
-
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ data: content });
   const result = await request;
-  console.log('Data saved to database', result.value);
+  console.log('Data saved to database', result);
 };
 
 export const getDb = async () => {
-  const txtEditorDB = await openDB('jate', 'readonly');
-  const storeVar = trasnVar.objectStore('jate');
-  const request = storeVar.get(1);
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get(1);
   const result = await request;
   result
-    ? console.log('Data retrieved from the database', result.value)
+    ? console.log('Data retrieved from the database', result)
     : console.log('Data not found in the database');
 };
 
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -30,8 +30,8 @@ module.exports = () => {
         fingerprints: false,
         inject: true,
         name: 'Just Another Text Editor',
-        short_name: 'J.A.T.E.',
-        description: "This application installs 'Just Another Text Editor' or 'JATE' for short.",
+        short_name: 'JATE',
+        description: "This application installs 'JATE'",
         background_color: '#225ca3',
         theme_color: '#225ca3',
         start_url: '/',
@@ -39,7 +39,7 @@ module.exports = () => {
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512], //Various image sizes
+            sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           }
         ]
